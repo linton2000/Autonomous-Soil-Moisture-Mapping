5f029c4b484236c9ec096be3d7ebd3dea50a15ed
Linton Charles <lintoncharles2000@gmail.com>
2023-05-18 17:43:52 +1000

    feat: Optical Depth Equation

--- LMEBRegressor.py
+++ LMEBRegressor.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ParamHandler import ParamHandler
 from Regressor import Regressor
 
@@ -15,4 +16,24 @@ class LMEBRegressor(Regressor):
         return self._simulate_TB()
     
     def _simulate_TB(self):
-        return self.params.param_dict['sand']
+        p = self.params.param_dict
+        theta = p['theta']
+        for p in ['h', 'v']:
+            tt, omega = [0]*2
+            b1s, b2s, lai = p['b1s'], p['b2s'], p['lai']
+            tau_lit, tau_inc = p['tau_lit'], p['tau_inc']
+            if p == 'h': 
+                tt = p['tth']
+                omega = p['omgh']
+            elif p == 'v': 
+                tt = p['ttv']
+                omega = p['omgv']
+            else: raise Exception(f"Invalid pol value '{p}' in _simulate_TB()")
+
+            # Total Optical Depth = Standing Vegetation + Canopy Bottom + Veg. Water Content
+            tau_veg = (b1s * lai * b2s) * (tt * np.sin(theta)**2 + np.cos(theta)**2)
+            tau = tau_veg + tau_lit + tau_inc
+
+            
+            gamma = np.exp(-tau / np.cos(theta))
+            tb = (1 - omega)*(1 - gamma)*(1 + gamma*refl_gr)*temp_cp + (1-refl_gr)*gamma*temp_gr

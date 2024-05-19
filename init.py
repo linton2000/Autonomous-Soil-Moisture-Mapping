37e5aa734866553d5b75e761a2f79279587f3077
Linton Charles <lintoncharles2000@gmail.com>
2024-05-19 17:13:04 +1000

    Clean root directory

--- init.py
+++ init.py
@@ -1,7 +1,3 @@
-from SMRetriever import SMRetriever
-from ParamHandler import ParamHandler
-
-if __name__ == '__main__':
-    params = ParamHandler(data_opt='tibobs')
-    retriever = SMRetriever(params=params)
-    retriever.plot_basic_ml()
+import scipy.io
+mat_data = scipy.io.loadmat('matlab_lmeb/geodata_r1.mat')
+print(mat_data['fdata'])
---
+++ lmeb-python/init.py
@@ -0,0 +1,7 @@
+from SMRetriever import SMRetriever
+from ParamHandler import ParamHandler
+
+if __name__ == '__main__':
+    params = ParamHandler(data_opt='tibobs')
+    retriever = SMRetriever(params=params)
+    retriever.plot_basic_ml()
